22738c1c4696b0e6d5071239e84fb37f2bcad5dd
xavier <xavierwork01@gmail.com>
2020-10-16 16:15:07 +0800

    10/16 last update

--- shoppingcart.js
+++ shoppingcart.js
@@ -67,19 +67,38 @@ var product = [
 
 // 購物車商品數量增加
 
+let userWelfare = parseInt($('.red').text())
+console.log(userWelfare)
+
+
 $('.plus-btn').click(function(){
     
     var mount = $(this).siblings('.mount-inp')
+
+    console.log(mount.val())
+
     if(mount.val() != ""){
 
         mount.val(parseInt(mount.val()) + 1)
-        var unit = parseInt($(this).parent('div').siblings('.unit-price').text().replace(',','').replace('$',''))
-        var sum = $(this).parent('div').siblings('.total-price')
-        sum.text('$' + formatprice(unit * parseInt(mount.val())))
+        
+        let unit = parseInt($(this).parent('div').siblings('div').children('.unit-price').text().replace(',','').replace('$',''))
+        console.log(unit)
+
+        let welfare = parseInt($(this).parent('div').siblings('div').children('.welfare-price').text().replace(',','').replace('$',''))
+        console.log(welfare)
+
+        let checkWelfare = $(this).parent('div').siblings('div').children('.welfare-inp')
+    
+        checkWelfare.val(welfare * mount.val())
+        console.log(checkWelfare)
+         
+        let sum = $(this).parent('div').siblings('div').children('.total-price')
+        console.log(sum)
+        sum.text('$' + formatprice((unit * parseInt(mount.val())) - parseInt(checkWelfare.val())))
 
         var add = 0
 
-        for(i=0;i < $('.total-price').length ; i++){
+        for(var i = 0;i < $('.total-price').length ; i++){
 
         
             add = add + parseInt($('.total-price').eq(i).text().replace(/,/g,'').replace('$',''))
@@ -97,17 +116,17 @@ $('.plus-btn').click(function(){
 
 $('.less-btn').click(function(){
 
-    var mount = $(this).siblings('.mount-inp')
+    let mount = $(this).siblings('.mount-inp')
     if(mount.val() > 1 ){
 
         mount.val(parseInt(mount.val()) - 1)
-        var unit = parseInt($(this).parent('div').siblings('.unit-price').text().replace(',','').replace('$',''))
-        var sum = $(this).parent('div').siblings('.total-price')
+        let unit = parseInt($(this).parent('div').siblings('.unit-price').text().replace(',','').replace('$',''))
+        let sum = $(this).parent('div').siblings('.total-price')
         sum.text('$' + formatprice(unit * parseInt(mount.val())))
 
-        var add = 0
+        let add = 0
 
-        for(i=0;i < $('.total-price').length ; i++){
+        for(var i = 0;i < $('.total-price').length ; i++){
 
         
             add = add + parseInt($('.total-price').eq(i).text().replace(/,/g,'').replace('$',''))
@@ -122,19 +141,20 @@ $('.less-btn').click(function(){
 
 // 商品數量變動處理
 
-var total = $('.shopcoupon-money')
+let total = $('.shopcoupon-money')
+let weltotal = 0;
 
 $('.mount-inp').on('keyup change',function(){
 
-    $(this).val($(this).val().replace(/[^1-9]\d{2}$/,''));
+    $(this).val($(this).val().replace(/^(0|[1-9][0-9]{3})/,''));
     if((event.which < 48 || event.which > 57)){
         event.preventDefault()
     }
 
     
-    var mount = $(this)
-    var unit = parseInt($(this).parent('div').siblings('.unit-price').text().replace(',','').replace('$',''))
-    var sum = $(this).parent('div').siblings('.total-price')
+    let mount = $(this)
+    let unit = parseInt($(this).parent('div').siblings('.unit-price').text().replace(',','').replace('$',''))
+    let sum = $(this).parent('div').siblings('.total-price')
     sum.text('$' + formatprice(unit * parseInt(mount.val())))
 
     if(mount.val() == '' || mount.val() == 0 ){
@@ -144,9 +164,9 @@ $('.mount-inp').on('keyup change',function(){
         
     }
 
-    var add = 0
+    let add = 0
 
-    for(i=0;i < $('.total-price').length ; i++){
+    for(var i = 0;i < $('.total-price').length ; i++){
 
         
         add = add + parseInt($('.total-price').eq(i).text().replace(/,/g,'').replace('$',''))
@@ -156,13 +176,57 @@ $('.mount-inp').on('keyup change',function(){
 
 })
 
-for(i=0;i < $('.total-price').length ; i++){
+for(var i = 0;i < $('.total-price').length ; i++){
         
-    var add = 0
+    let add = 0
     add = add + parseInt($('.total-price').eq(i).text().replace(/,/g,'').replace('$',''))
     total.text('$' +　formatprice(add))
 
 }
+
+// 福利金變動處理
+
+$('.welfare-inp').on('change keyup',function(){
+
+    // $(this).val($(this).val().replace(/^(0|[1-9][0-9]{n})$/,''));
+    // if((event.which < 48 || event.which > 57)){
+    //     event.preventDefault()
+    // }
+
+    // $(this).val().replace(/[^/d]/g,'')
+
+    // if($(this).val().length == 0 && $(this).val() == 0){
+
+    //     $(this).val('')
+
+    // }
+
+    
+    let weladd = 0
+
+    for(var j = 0;j < $('.welfare-inp').length; j++){
+
+        
+        weladd = weladd + parseInt($('.welfare-inp').eq(j).val())
+
+    }
+
+    weltotal = weladd
+
+    console.log(weltotal)
+
+    if(weltotal > userWelfare){
+
+        $('.modal-welfare').modal('show')
+        $(this).val('0')
+
+    }
+
+})
+
+
+
+
 
 // 商品全選 & 折價券全選
 
@@ -196,7 +260,84 @@ function formatprice(money){
 
 }
 
-// var tom = 166666666;
 
-// console.log(formatprice(tom).replace(/,/g,''))
+// 選擇折價券選取樣式
+
+$('.coupon-sales').on('click',function(){
+
+    if($(this).find('.chk-coupon-use i').css('display') == 'none'){
+
+        $(this).find('.chk-coupon-use i').css('display','flex')
+        $(this).find('.ticket-head h3').css({'color':'#fff','text-shadow':'1px 1px 1px gray'})
+
+    } else {
+
+        $(this).find('.chk-coupon-use i').css('display','none')
+        $(this).find('.ticket-head h3').css({'color':'gray','text-shadow':'-1px -1px 1px #000'})
+
+    }
+
+})
+
+
+
+// 輸入優惠瑪判斷
+
+$('.coupon-num-use').on('click',function(){
+
+    let number = $('.coupon-num-inp').val()
+
+    if(number == 1){
+
+        $('.coupon-addbox').append(
+            `<div class="coupon-use">
+                <div class="coupon-sales">
+                    <div class="ticket-head">
+                        <h3>TvPay</h3>
+                    </div>
+                    <div class="ticket-body">
+                        <div class="ticket-name1">
+                            <div>8折 低消$0 最高折 $50</div>
+                            <p>即將到期 剩下12小時</p>
+                        </div>
+                        <div class="chk-coupon-use"><i class="fas fa-check"></i></div>
+                        <div class="ticket-name2">
+                            <a href="#" class="caption-open">使用規則</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        )
+
+        $('.coupon-add p').text('')
+
+    } else if(number == 2){
+
+        $('.coupon-add p').text('無此優惠碼或優惠已過期!')
+
+
+    }
+    
+
+})
+
+
+// 輸入優惠瑪裡的優惠券選取樣式
+
+$('.coupon-addbox').on('click','.coupon-sales',function(){
+
+    if($(this).find('.chk-coupon-use i').css('display') == 'none'){
+
+        $(this).find('.chk-coupon-use i').css('display','flex')
+        $(this).find('.ticket-head h3').css({'color':'#fff','text-shadow':'1px 1px 1px gray'})
+
+    } else {
+
+        $(this).find('.chk-coupon-use i').css('display','none')
+        $(this).find('.ticket-head h3').css({'color':'gray','text-shadow':'-1px -1px 1px #000'})
+
+    }
+
+})
+
 
